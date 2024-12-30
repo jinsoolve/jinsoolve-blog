3501793964f7a2c89184f1d2a04dc15e8bef7598
jinsoolve <63998352+jinsoolve@users.noreply.github.com>
2024-12-31 00:44:31 +0900

    댓글 테마 변경

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -1,13 +1,17 @@
 import { Box, useColorMode } from "@chakra-ui/react";
-import React from "react";
-import { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 
 const COMMENTS_ID = "comments-container";
 
 const Giscus = (): JSX.Element => {
   const { colorMode } = useColorMode();
 
-  const LoadComments = useCallback(() => {
+  const loadComments = useCallback(() => {
+    // 기존 Giscus 위젯 제거
+    const comments = document.getElementById(COMMENTS_ID);
+    if (comments) comments.innerHTML = "";
+
+    // 새로운 스크립트 추가
     const script = document.createElement("script");
     script.src = "https://giscus.app/client.js";
     script.setAttribute("data-repo", "jinsoolve/jinsoolve-blog.comment");
@@ -19,24 +23,17 @@ const Giscus = (): JSX.Element => {
     script.setAttribute("data-reactions-enabled", "1");
     script.setAttribute("data-emit-metadata", "0");
     script.setAttribute("data-input-position", "bottom");
-    script.setAttribute("data-theme", "preferred_color_scheme");
+    script.setAttribute("data-theme", colorMode === "dark" ? "dark_protanopia" : "light_protanopia");
     script.setAttribute("data-lang", "ko");
     script.setAttribute("crossorigin", "anonymous");
     script.async = true;
 
-    const comments = document.getElementById(COMMENTS_ID);
     if (comments) comments.appendChild(script);
-
-    return () => {
-      const comments = document.getElementById(COMMENTS_ID);
-      if (comments) comments.innerHTML = "";
-    };
   }, [colorMode]);
 
-  // Reload on theme change
   useEffect(() => {
-    LoadComments();
-  }, [LoadComments]);
+    loadComments();
+  }, [loadComments]);
 
   return <Box mt="100px" className="giscus" id={COMMENTS_ID} />;
 };
